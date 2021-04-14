bc3ca5fd738867564a5bdcc9191df5bd0d010c73
Mergen Battur <mergen@chowlyinc.com>
2021-04-14 05:44:28 -0500

    finish 31 as

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,9 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def about
-  end
+  def about; end
 
-  def contact
-  end
+  def contact; end
+
+  def faq; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/about'
   get 'welcome/contact'
+  get 'welcome/faq'
 
 end
