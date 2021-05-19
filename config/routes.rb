949b727c38f331a3895b2aee9ccd0b4bb4a9e3cd
Mergen Battur <mergen@chowlyinc.com>
2021-05-19 14:18:38 -0500

    remove 38 a

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :topics do
     resources :posts, except: [:index]
   end
-=======
-  resources :questions
-  resources :posts
->>>>>>> 710b56737820b65dc653618cfefa72fc71c81376
 
   get 'about' => 'welcome#about'
 
