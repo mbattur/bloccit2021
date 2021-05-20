113366736c53872fb12a238fffe47895e620b629
Mergen Battur <mergen@chowlyinc.com>
2021-05-20 11:11:27 -0500

    finish 43 c

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_205838) do
+ActiveRecord::Schema.define(version: 2021_05_19_192846) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -49,6 +49,14 @@ ActiveRecord::Schema.define(version: 2021_05_17_205838) do
     t.string "name"
     t.boolean "public", default: true
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
