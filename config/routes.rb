39571ef5154e6cdabc465c34638250510b08e3d2
Mergen Battur <mergen@chowlyinc.com>
2021-06-02 07:15:10 -0500

    finish 45 c

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
-  resources :users, only: [:new, :create]
+  resources :users, only: %i[new create]
+
+  resources :sessions, only: %i[new create destroy]
 
   get 'about' => 'welcome#about'
 
